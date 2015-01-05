5fdf58615140810856923f014acb476d1a041f08
Johan Frick <johan.frick@mobenga.com>
2015-01-05 21:34:43 +0100

    Use paper-shadow

--- client/features/main/foto/view-model.js
+++ client/features/main/foto/view-model.js
@@ -1,6 +1,14 @@
 var viewModel = {
     init: function () {
-        this.image = ko.observable('img/affisch_300.jpg');
+        var images = [
+            {url: 'affisch_300', width: '239px', height: '300px'},
+            {url: 'affisch_300', width: '239px', height: '300px'}
+        ];
+        images.forEach(function (image) {
+            image.url = 'url(img/' + image.url + '.jpg)';
+
+        });
+        this.images = images;
     }
 };
 
