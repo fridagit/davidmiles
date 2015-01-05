5ccbda5d8953c88ce63b60f0c90a760e675620be
Johan Frick <johan.frick@mobenga.com>
2015-01-05 18:38:53 +0100

    Changed menu to use Material Design's scaffold

--- client/features/main/view-model.js
+++ client/features/main/view-model.js
@@ -8,9 +8,49 @@ var viewModel = {
         bus.subscribe('main-content', function(item) {
             self.mainContent(item.data.toLowerCase());
         });
+        createSections(this);
 
     }
 };
+
+function createSections(self) {
+    var sections = [
+        {
+            text: 'Foto',
+            selected: true
+        },
+        {
+            text: 'Album'
+        },
+        {
+            text: 'Video'
+        }
+    ];
+
+    self.sections = sections.map(function(section) {
+        return {
+            text: ko.observable(section.text),
+            selected: ko.observable(false),
+            select: function(current) {
+                self.sections.forEach(function(s){
+                    s.selected(false);
+                });
+                localStorage.setItem('selected-section', current.text());
+                current.selected(true);
+                bus.publish('main-content', current.text().toLowerCase());
+            }
+        };
+    });
+    var selected = localStorage.getItem('selected-section') || 'Foto';
+    self.selectedIndex = ko.observable(0);
+    self.sections.forEach(function(section, index) {
+        if (section.text() === selected) {
+            section.selected(true);
+            self.selectedIndex(index);
+        }
+    });
+}
+
 
 module.exports.create = function () {
     var vm = Object.create(viewModel);
--- client/features/start/index.js
+++ client/features/start/index.js
@@ -1,17 +1,11 @@
 var router = require('router');
-var layout = require('layout-engine');
-
-var header = require('header');
 var main = require('main');
-
 
 exports.start = function () {
     router.on('/', function (ctx) {
-            header.render();
             main.render();
         }
     );
 };
-
 
 exports.controllers = [];
