996dae9807541654aa5171b387a9e0d3b540f9e8
Johan Frick <johan.frick@mobenga.com>
2015-01-08 10:26:06 +0100

    Gulpify (also removed HAM logo)

--- client/grunt/tasks/alias.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (grunt) {
-
-	// Build product
-	grunt.registerTask('build', ['jshint', 'clean:web', 'build-product']);
-
-	// Development mode, watch all files and rebuild + run tests
-	grunt.registerTask('develop', ['build', 'watch:all']);
-
-};
---
+++ client/gulpfile.js
@@ -0,0 +1,149 @@
+var gulp = require('gulp');
+var path = require('path');
+var runSequence = require('run-sequence');
+var stylish = require('jshint-stylish');
+
+var jshint = require('gulp-jshint');
+var rimraf = require('gulp-rimraf');
+var size = require('gulp-size');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var less = require('gulp-less');
+var gulpIf = require('gulp-if');
+var sourcemaps = require('gulp-sourcemaps');
+
+var bundleGears = require('mobenga-bundle-gears');
+var conflict = require('mobenga-check-conflict');
+var handlebarsStream = require('mobenga-handlebars-stream');
+
+var pkg = require('./package.json');
+
+var node_env = process.env.NODE_ENV || 'development';
+
+console.log('Running gulp for environment: ' + node_env);
+
+var buildsDir = 'builds/web/' + node_env;
+
+gulp.task('build', function (callback) {
+	runSequence('clean',
+		'lint',
+        'copy-lib',
+		'bundle',
+		['copy', 'less'],
+		'template',
+		callback);
+});
+
+
+gulp.task('copy-lib', function(callback) {
+    var files = ['node_modules/cogwheels/builds/cogwheels-*.js',
+        'node_modules/cogwheels/lib/knockout*.js',
+        '!node_modules/cogwheels/lib/knockout*min.js',
+        '!node_modules/cogwheels/builds/cogwheels-*min.js',
+		'node_modules/knockout.mapping/knockout.mapping.js',
+		'node_modules/jquery/dist/jquery.js'];
+
+    var destDir = buildsDir + '/lib';
+
+    return gulp.src(files).pipe(gulp.dest(destDir));
+});
+
+gulp.task('bundle', function (callback) {
+	runSequence('conflict',
+		['gears', 'gears:shared'],
+		callback);
+});
+
+gulp.task('default', ['build']);
+
+gulp.task('lint', function () {
+	return gulp.src('gears/**/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter(stylish, {
+			fail: true
+		}))
+		.pipe(jshint.reporter('fail'));
+});
+
+gulp.task('watch', ['build'], function () {
+
+	gulp.watch('web/**', ['build']);
+	gulp.watch('gears/**', ['build']);
+});
+
+gulp.task('clean', function () {
+	return gulp.src(buildsDir, {
+		read: false
+	}).pipe(rimraf());
+});
+
+gulp.task('copy', function () {
+	return gulp.src([
+		'web/**/*'
+	]).pipe(gulp.dest(buildsDir));
+
+});
+
+gulp.task('conflict', function () {
+
+	return gulp.src(['gears/**/feature.json'])
+		.pipe(conflict());
+});
+
+gulp.task('less', function () {
+	gulp.src(['web/less/main.less', 'gears/**/*.less'])
+		.pipe(size({title: 'less', 'showFiles': true}))
+		.pipe(less())
+		.pipe(concat('main.css'))
+		.pipe(gulp.dest(buildsDir + '/css'))
+});
+
+gulp.task('gears', function () {
+
+	var gearDirs = ['gears'];
+	var gearsDest = buildsDir + '/gears';
+	var htmlMin = {
+		collapseWhitespace: true,
+		removeComments: false
+	};
+
+	return bundleGears.source(gearDirs, {htmlMin: htmlMin})
+		.pipe(size({title: 'js', showFiles: true}))
+		.pipe(sourcemaps.init())
+			.pipe(concat('gears.js'))
+			.pipe(rename({suffix: '-v' + pkg.version}))
+			.pipe(gulpIf(node_env !== 'development', uglify()))
+		.pipe(sourcemaps.write('../maps'))
+		.pipe(gulp.dest(gearsDest));
+});
+
+gulp.task('gears:shared', function () {
+	var htmlMin = {
+		collapseWhitespace: true,
+		removeComments: false
+	};
+
+	return bundleGears.source(['node_modules'], {htmlMin: htmlMin})
+		.pipe(size({title: 'js', showFiles: true}))
+		.pipe(concat('shared-gears.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest(buildsDir));
+});
+
+gulp.task('template', function () {
+	var opts = {
+		cwd: buildsDir
+	};
+
+	var data = {
+		libs: { files: ['lib/*.js', '!lib/cogwheels*'], opts: opts },
+		css: { files: ['css/*.css'], opts: opts },
+		gears: { files: ['gears/gears-v' + pkg.version + '.js'], opts: opts },
+		cogwheels: { files: ['lib/cogwheels-v*.js'], opts: opts }
+	};
+
+	return handlebarsStream('web/index.template.html', data)
+		.pipe(rename('index.html'))
+		.pipe(gulp.dest(buildsDir));
+});
