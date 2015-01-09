293cee85529f57609c17e87d9c62608ad22f38ef
Johan Frick <johan.frick@mobenga.com>
2015-01-10 00:51:32 +0100

    referencer

--- client/gears/main/referenser/view-model.js
+++ client/gears/main/referenser/view-model.js
@@ -1,5 +1,13 @@
+var request = require('data-request');
+
 module.exports = {
     init: function () {
-
+        this.references = ko.observableArray();
+        request.getJson('referenser', function(references) {
+            references.forEach(function(reference) {
+               reference.text = '”' + reference.text + '”';
+            });
+            this.references(references);
+        }.bind(this));
     }
 };
