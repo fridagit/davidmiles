f0779ab1686a8ca97af9a1e819e06d6b8882ba4c
Johan Frick <johan.frick@mobenga.com>
2015-01-19 01:13:55 +0100

    Artist video order

--- client/gears/main/video/view-model.js
+++ client/gears/main/video/view-model.js
@@ -1,22 +1,28 @@
 var request = require('utils/request');
+var dataRequest = require('data-request');
+
 
 module.exports = {
     init: function () {
-        var ids = ['34ZkmRl8l0k', 'WDnrjwN5X5I', 'ABF7PrYchNQ', 'AXf7meAEQnU'];
-        this.videos = ids.map(function (id) {
-            var video = {
-                id: id,
-                iFrameUrl: 'https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&origin=http://davidmiles.se',
-                image: 'url(http://i.ytimg.com/vi/' + id + '/mqdefault.jpg)',
-                title: ko.observable(),
-                width: '320px',
-                height: '180px',
-                fill: true
-            };
-            request('GET', 'http://gdata.youtube.com/feeds/api/videos/' + video.id + '?alt=json').end(function (result) {
-                video.title(result.body.entry.title.$t);
+        var self = this;
+        this.videos = ko.observableArray();
+        dataRequest.getJson('video-artist', function (ids) {
+            var videos = ids.map(function (id) {
+                var video = {
+                    id: id,
+                    iFrameUrl: 'https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&origin=http://davidmiles.se',
+                    image: 'url(http://i.ytimg.com/vi/' + id + '/mqdefault.jpg)',
+                    title: ko.observable(),
+                    width: '320px',
+                    height: '180px',
+                    fill: true
+                };
+                request('GET', 'http://gdata.youtube.com/feeds/api/videos/' + video.id + '?alt=json').end(function (result) {
+                    video.title(result.body.entry.title.$t);
+                });
+                return video;
             });
-            return video;
+            self.videos(videos);
         });
     }
 };
