793955d276cd2309f19f9ae76c8e4dce9ae49409
Johan Frick <johan.frick@mobenga.com>
2015-01-22 04:57:07 +0100

    remove unused feature: loading-indicator

--- client/gears/loading-indicator/index.js
+++
@@ -1,7 +0,0 @@
-exports.init = function () {
-	ko.components.register('loading', component);
-};
-
-var component = {
-	template: '<div data-bind="if: visible" style="text-align: center"><img src="img/loading.gif"></div>'
-};
