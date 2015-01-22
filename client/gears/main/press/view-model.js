44d47df069d0a7606b167e6cec165292fd4bfaaa
Johan Frick <johan.frick@mobenga.com>
2015-01-22 05:25:41 +0100

    Press: styling and download link for images

--- client/gears/main/press/view-model.js
+++ client/gears/main/press/view-model.js
@@ -2,10 +2,12 @@ module.exports = {
     init: function () {
         var images = [];
         for (var i = 1; i <= 22; i++) {
+            var fullUrl = 'img/press/david_press_' + i + '.jpg';
             images.push(
                 {
                     thumb: 'img/press/david_press_' + i + '_thumbnail.jpg',
-                    url: 'img/press/david_press_' + i + '.jpg'
+                    url: fullUrl,
+                    downloadLink: '<a href="' + fullUrl + '" target="_blank">Klicka här för att ladda ner</a>'
                 }
             );
         }
