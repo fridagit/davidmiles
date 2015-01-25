797854af257c5c31d2e76c886a622a60c784c985
Johan Frick <johan.frick@mobenga.com>
2015-01-25 15:33:53 +0100

    Podcasts didn't work for mobiles

--- client/gears/main/podcast/view-model.js
+++ client/gears/main/podcast/view-model.js
@@ -7,17 +7,7 @@ module.exports = {
         self.selectedUrl = ko.observable();
         request.getJson('podcast', function (podcasts) {
             podcasts.forEach(function (podcast) {
-                var url = 'http://sjowallmiles.podomatic.com/embed/frame/posting/' + podcast.podomaticId +
-                    '?json_url=http%3A%2F%2Fsjowallmiles.podomatic.com%2Fentry%2Fembed_params%2F' + podcast.podomaticId +
-                    '%3Fcolor%3D43bee7%26autoPlay%3Dtrue%26width%3D4500%26height%3D85%26objembed%3D0';
-                podcast.click = function () {
-                    $('.iFrameLoading').slideDown(function () {
-                        self.selectedUrl(url);
-                    });
-                };
-                podcast.selected = ko.computed(function () {
-                    return self.selectedUrl() === url;
-                });
+                podcast.url = 'http://sjowallmiles.podomatic.com/entry/' + podcast.podomaticId;
             });
             self.podcasts(podcasts);
         });
