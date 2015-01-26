470ace9e39a9001e2331b7c782cf040db73b6f11
Johan Frick <johan.frick@mobenga.com>
2015-01-26 12:49:57 +0100

    Jsonify reviews page

--- client/gears/main/recensioner/view-model.js
+++ client/gears/main/recensioner/view-model.js
@@ -1,5 +1,11 @@
+var request = require('data-request');
+
 module.exports = {
     init: function () {
-
+        var self = this;
+        this.albums = ko.observableArray();
+        request.getJson('recensioner', function (albums) {
+            self.albums(albums);
+        });
     }
 };
