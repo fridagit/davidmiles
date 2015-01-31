b59d610fba537e90bbef61ea58011fbb27addd53
Johan Frick <johan.frick@mobenga.com>
2015-01-31 20:04:48 +0100

    Improve discography

--- client/gears/main/discografi/view-model.js
+++ client/gears/main/discografi/view-model.js
@@ -4,18 +4,22 @@ module.exports = {
     init: function () {
         var self = this;
         self.albums = ko.observableArray();
-        request.getJson('spotify_albums', function (ids) {
-            ids.forEach(function (id) {
-                var album = {
-                    id: id,
-                    iFrameUrl: 'https://embed.spotify.com/?uri=spotify:album:' + id + '&view=coverart&theme=white',
-                    title: '',
-                    image: ko.observable(),
-                    width: '250px',
-                    height: '250px'
-                };
-                request.get('https://api.spotify.com/v1/albums/' + album.id, function (result) {
+        request.getJson('spotify_albums', function (albums) {
+            albums.forEach(function (album) {
+                album.spotifyUrl = 'http://open.spotify.com/album/' + album.spotifyId;
+                album.iTunesUrl = 'https://itunes.apple.com/us/album/' + album.iTunesId;
+                album.shopUrl = album.shopId ? 'http://davidmiles.tictail.com/product/' + album.shopId : false;
+                album.title = ko.observable();
+                album.date = ko.observable();
+                album.trackCount = ko.observable();
+                album.image = ko.observable();
+                album.width = '250px';
+                album.height = '250px';
+                request.get('https://api.spotify.com/v1/albums/' + album.spotifyId, function (result) {
                     var images = result.body.images;
+                    album.title(result.body.name);
+                    album.date(result.body['release_date'].replace(/-.*/, ''));
+                    album.trackCount(result.body.tracks.items.length);
                     images.forEach(function (image) {
                         if (image.height === 300) {
                             album.image('url(' + image.url + ')');
