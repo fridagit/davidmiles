4d8e40089dc2c947b1c04cd37777668839b68cbb
Johan Frick <johan.frick@mobenga.com>
2015-02-06 07:48:05 +0100

    Urls to each page

--- client/gears/main/view-model.js
+++ client/gears/main/view-model.js
@@ -1,5 +1,6 @@
 var bus = require('message-bus');
 var request = require('data-request');
+var router = require('router');
 
 var viewModel = {
     init: function () {
@@ -17,32 +18,30 @@ var viewModel = {
         request.getJson('menu', function (sections) {
             var selected = localStorage.getItem('selected-section');
             sections.forEach(function (section, index) {
-                section.selected = ko.observable(false);
                 section.initPage = ko.observable(false);
                 section.header = section.header || false;
                 section.id = section.id || section.text.toLowerCase();
+                section.selected = ko.computed(function() {
+                    return section.id === self.mainContent();
+                });
                 section.select = function () {
                     if (!section.header) {
-                        self.sections().forEach(function (s) {
-                            s.selected(false);
-                        });
-                        localStorage.setItem('selected-section', section.id);
-                        section.selected(true);
-                        bus.publish('main-content', section.id);
+                        router.navigate('/' + section.id);
                         document.querySelector('#sidebar').parentElement.className = 'row row-offcanvas row-offcanvas-left';
                     }
                 };
-                if (selected) {
-                    if (section.id === selected) {
+                if (!selected) {
+                    if (section.startPage) {
                         section.select();
                         section.initPage(true);
                     }
-                } else if (section.startPage) {
-                    section.select();
-                    section.initPage(true);
                 }
                 self.sections.push(section);
             });
+            if (selected)  {
+                bus.publish('main-content', selected);
+            }
+
         });
 
     }
--- client/gears/start/index.js
+++ client/gears/start/index.js
@@ -1,9 +1,22 @@
 var router = require('router');
 var main = require('main');
+var bus = require('message-bus');
 
 exports.start = function () {
+    var firstRender = true;
     router.on('/', function (ctx) {
             main.render();
+            firstRender = false;
+        }
+    );
+    router.on('/:page', function (ctx) {
+            if (!firstRender) {
+                bus.publish('main-content', ctx.params.page);
+            } else {
+                firstRender = false;
+                localStorage.setItem('selected-section', ctx.params.page);
+                main.render();
+            }
         }
     );
 };
