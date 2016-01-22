694bab4a6cc2efd3f9bfefd53c3753547540686c
Johan Frick <johan.frick@mobenga.com>
2016-01-22 14:30:57 +0100

    Editable lyrics, everything in one file: lyrics.txt

--- client/gears/main/texter/view-model.js
+++ client/gears/main/texter/view-model.js
@@ -5,27 +5,45 @@ module.exports = {
         var self = this;
         this.lyrics = ko.observableArray();
         self.selected = ko.observable();
-        request.getJson('texter', function (lyricsList) {
-            lyricsList.forEach(function (lyricsItem) {
-                request.getLyrics(lyricsItem.textFile, function (text) {
-                    lyricsItem.fileName = 'texter/' + lyricsItem.textFile;
-                    lyricsItem.originalText = ko.observable(text);
-                    lyricsItem.editMode = ko.observable(false);
-                    lyricsItem.text = text.replace(/(?:\r\n|\r|\n)/g, '<br>');
-                    lyricsItem.selected = ko.observable(false);
-                    lyricsItem.click = function () {
-                        if (lyricsItem.selected()) {
-                            lyricsItem.selected(false);
-                        } else {
-                            self.lyrics().forEach(function (item) {
-                                item.selected(false);
-                            });
-                            lyricsItem.selected(true);
-                        }
-                    };
-                    self.lyrics.push(lyricsItem);
-                });
+        self.allTexts = ko.observable('');
+        self.editMode = ko.observable(false);
+        request.getTxt('lyrics', function (allTexts) {
+            self.allTexts(allTexts);
+            var currentLyricsItem;
+            allTexts.split(/(?:\r\n|\r|\n)/g).forEach(function (line) {
+                if (line.trim().indexOf('#') === 0) {
+                    if (currentLyricsItem !== undefined) {
+                        self.lyrics.push(currentLyricsItem);
+                    }
+                    var title = line.trim().substr(1).trim();
+                    currentLyricsItem = self.newLyricsItem(title);
+                }
+                else {
+                    if (currentLyricsItem !== undefined) {
+                        currentLyricsItem.text = currentLyricsItem.text + line + '\n';
+                    }
+                }
             });
         });
+
+    },
+    newLyricsItem: function (title) {
+        var lyricsItem = {};
+        lyricsItem.text = '';
+        lyricsItem.title = title;
+        lyricsItem.selected = ko.observable(false);
+        var self = this;
+        lyricsItem.click = function () {
+            if (lyricsItem.selected()) {
+                lyricsItem.selected(false);
+            } else {
+                self.lyrics().forEach(function (item) {
+                    item.selected(false);
+                });
+                lyricsItem.selected(true);
+            }
+        };
+        return lyricsItem;
     }
-};
+}
+;
