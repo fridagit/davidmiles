b6498f89562e5f095a78652a69bd215b2ae16e9f
Johan Frick <johan.frick@mobenga.com>
2016-05-16 21:44:05 +0200

    Improve start page

--- client/gears/main/hem_160516/view-model.js
+++ client/gears/main/hem_160516/view-model.js
@@ -10,16 +10,15 @@ module.exports = {
             id: id,
             iFrameUrl: 'https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&origin=http://davidmiles.se',
             image: 'url(http://i.ytimg.com/vi/' + id + '/mqdefault.jpg)',
-            title: ko.observable(),
+            title: '',
             width: '320px',
             height: '180px',
             fill: true
         };
-        request('GET', 'https://www.googleapis.com/youtube/v3/videos?key=' + apiKey +
-            '&fields=items(snippet(title))&part=snippet&id=' + video.id).end(function (result) {
-            video.title(result.body.items[0].snippet.title);
-        });
         self.videos([video]);
 
+    },
+    click: function() {
+        window.location.href='https://open.spotify.com/track/3u4Z8W600ugcCeHijyykBE';
     }
 };
