ceeea2addfd9cd7557f826cb03fcbe913cf8a42e
Johan Frick <johan.frick@mobenga.com>
2016-05-31 20:52:34 +0200

    Förslag på sida 1/6

---
+++ client/gears/main/hem_160601/view-model.js
@@ -0,0 +1,23 @@
+var request = require('utils/request');
+
+module.exports = {
+    init: function () {
+        var self = this;
+        this.videos = ko.observableArray();
+        var id = 'u05Hcpy6ixs';
+        var video = {
+            id: id,
+            iFrameUrl: 'https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&origin=http://davidmiles.se',
+            image: 'url(http://i.ytimg.com/vi/' + id + '/mqdefault.jpg)',
+            title: '',
+            width: '320px',
+            height: '180px',
+            fill: true
+        };
+        self.videos([video]);
+
+    },
+    click: function() {
+        window.location.href='https://open.spotify.com/track/3u4Z8W600ugcCeHijyykBE';
+    }
+};
