f961387856ad798f3c45457abf2adbad8c2d39ca
Johan Frick <johan.frick@mobenga.com>
2016-06-20 11:34:44 +0200

    Use more https

--- client/gears/pages/hem_160601/view-model.js
+++ client/gears/pages/hem_160601/view-model.js
@@ -8,7 +8,7 @@ module.exports = {
         var video = {
             id: id,
             iFrameUrl: 'https://www.youtube.com/embed/' + id + '?autoplay=1&autohide=1&origin=http://davidmiles.se',
-            image: 'url(http://i.ytimg.com/vi/' + id + '/mqdefault.jpg)',
+            image: 'url(https://i.ytimg.com/vi/' + id + '/mqdefault.jpg)',
             title: '',
             width: '320px',
             height: '180px',
