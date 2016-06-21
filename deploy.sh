8830cb56f22ea439e849038372bb53c59df9cc08
Johan Frick <johan.frick@mobenga.com>
2016-06-21 21:18:56 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ CLIENT_ROOT="/Users/jofr/Development/davidmiles/client/"
 SRC_DIR=$CLIENT_ROOT"builds/web/development/"
 DEST_DIR="davidmiles.se@ssh.davidmiles.se:/www/"
 
-rsync --archive --delete $DEST_DIR"texter" $CLIENT_ROOT"web/"
+rsync --archive --delete $DEST_DIR"txt" $CLIENT_ROOT"web/"
 rsync --archive --delete $DEST_DIR"json" $CLIENT_ROOT"web/"
 rsync --archive --ignore-existing -- $DEST_DIR"img" $CLIENT_ROOT"web/"
 
