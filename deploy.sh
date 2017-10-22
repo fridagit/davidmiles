42a8d1d4e7d77271cdb60b244dd810c0d4a7f577
Johan Frick <johan.frick@playtech.com>
2017-10-22 21:27:20 +0300

    Fix bower dependency

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,4 @@ CLIENT_ROOT="client/"
 SRC_DIR=$CLIENT_ROOT"builds/web/development/"
 DEST_DIR="davidmiles.se@ssh.davidmiles.se:/www/"
 
-rsync --archive --delete $DEST_DIR"txt" $CLIENT_ROOT"web/"
-rsync --archive --delete $DEST_DIR"json" $CLIENT_ROOT"web/"
-rsync --archive --ignore-existing -- $DEST_DIR"img" $CLIENT_ROOT"web/"
-
-echo "Ready to upload? Press enter to perform dry-run. Otherwise press ctrl+C."
-read
-rsync -n --verbose --archive --delete --chmod=Du=rwx,go=rx,Fu=rwx,og=rx  $SRC_DIR $DEST_DIR |grep -v "components/"
-echo "Dry run looks ok? Otherwise press ctrl+c"
-read
 rsync --archive --delete --chmod=Du=rwx,go=rx,Fu=rwx,og=rx  $SRC_DIR $DEST_DIR
